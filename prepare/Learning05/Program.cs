6d7d30507767b6a30cf5f5db891353f946233f65
janZ <janzander96@gmail.com>
2023-11-06 08:33:18 +0800

    finished shape assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Shape square = new Square("red", 3.3);
+
+        Shape rectangle = new Rectangle("yellow", 4.2, 3.5);
+
+        Shape circle = new Circle("blue", 10.0);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes){
+            shape.GetColor();
+            shape.GetArea();
+        }
     }
 }
